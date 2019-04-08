9b3c07debeee43634397398d10a755b9dc5473b9
Ryan Ly <rly@princeton.edu>
2019-04-07 22:39:00 -0700

    add tools to path

--- config.m
+++ config.m
@@ -2,7 +2,7 @@
 function config()
 
 addpath(fullfile(pwd));
-addpath(genpath(fullfile(pwd, 'helper')));
+addpath(genpath(fullfile(pwd, 'tools')));
 addpath(genpath(fullfile(pwd, 'util')));
 addpath(genpath(fullfile(pwd, 'matlab_sdk')));
 
