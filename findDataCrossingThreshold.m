b6695523ef8a8d933584c7632ac6293c4357aec4
Ryan Ly <rly@princeton.edu>
2019-04-07 23:15:13 -0700

    refactoring, cleaning up code, and commenting

--- findDataCrossingThreshold.m
+++ findDataCrossingThreshold.m
@@ -1,18 +1,36 @@
 function isExtreme = findDataCrossingThreshold(data, numSDsThresh, ...
         preExtremeSamples, postExtremeSamples, isUseMAD)
-% returns logical vector where the element is true if the corresponding
-% data point is either preExtremeSamples prior to an extreme data point or
-% postExtremeSamples after an extreme data point, as defined by
-% numSDsThresh standard deviations away from the mean, regardless of
-% positive or negative direction. in other words, each extreme data point
-% creates a segment of true values in the isExtreme vector with
-% preExtremeSamples prior and postExtremeSamples after the data point.
+% Label the data that is more than x SDs away from the mean with a pre- and
+% post-extreme value buffer
+% 
+% Inputs:
+% - data:               N x 1 vector of high-pass filtered data
+% - numSDsThresh:       number of SDs away from the mean to consider the
+%                       data an extreme value, i.e. part of a spike
+% - preExtremeSamples:  number of samples to extract prior to the extremal
+%                       value (trough/peak) corresponding to the threshold 
+%                       crossing
+% - postExtremeSamples: number of samples to extract after the extremal
+%                       value (trough/peak) corresponding to the threshold 
+%                       crossing
+% 
+% Outputs: 
+% - isExtreme:          N x 1 logical vector indicating whether the
+%                       corresponding value in data was above or below 
+%                       threshold (considers both), or is 
+%                       preExtremeSamples prior to an extreme data point or
+%                       postExtremeSamples after an extreme data point. in 
+%                       other words, each extreme data point creates a 
+%                       segment of true values in the isExtreme vector with
+%                       preExtremeSamples prior and postExtremeSamples 
+%                       after the data point
 
 data = makeRowVector(data);
 
-% compute noise level
+% compute mean and noise level
 meanData = nanmean(data);
-if isUseMAD % median absolute deviation - more robust than SD
+if isUseMAD 
+    % median absolute deviation - more robust than SD
     sdData = mad(data, 1);
 else
     sdData = nanstd(data);
@@ -22,6 +40,9 @@ lowerThresh = meanData - numSDsThresh * sdData;
 upperThresh = meanData + numSDsThresh * sdData;
 
 extremeInds = find(data < lowerThresh | data > upperThresh);
+
+% mark data points as extreme around the points that are above the upper
+% threshold or below the lower threshold
 isExtreme = false(size(data));
 for i = 1:numel(extremeInds)
     lb = max(1, extremeInds(i) - preExtremeSamples); % keep inds within range
